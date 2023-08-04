6598214d19095b9f3ab52590f7e239b8fd81573b
MatíasJMaranga <marangamatias@gmail.com>
2023-08-04 20:30:15 -0300

    Axios3

--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ app.use(cors({
     credentials: true
 }));
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://club-manager-backend-m4ol.onrender.com');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://club-manager-backend-m4ol.onrender.com/',
   withCredentials: true,
+  
 });
 
 export default instance;
