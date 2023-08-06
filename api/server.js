f816475f896bf33bc4c82a3a898cc7da67fc4006
MatíasJMaranga <marangamatias@gmail.com>
2023-08-06 20:47:02 -0300

    tryingSomeCorsConfig

--- api/server.js
+++ api/server.js
@@ -18,9 +18,11 @@ app.use(helmet());
 
 
 app.use(cors({
-    origin: [ 'https://club-manager-admin.netlify.app', 'https://club-manager-client.netlify.app', 'http://localhost:5173', 'http://localhost:5174' ],
+    origin: ['https://club-manager-admin.netlify.app', 'https://club-manager-client.netlify.app', 'http://localhost:5173', 'http://localhost:5174'],
     credentials: true,
-}));
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+  }));
 
 app.use(session(config.sessionConfig));
 app.use(passport.initialize());
