7f67a1e940a518be32bbfb7d9148e35fd0af544b
MatíasJMaranga <marangamatias@gmail.com>
2023-09-16 12:36:56 -0300

    satTest

--- api/config/config.js
+++ api/config/config.js
@@ -21,7 +21,9 @@ export const sessionConfig = {
   //Esta conf de cookie es solo para deploy, comentar en local sin req.isAuthticated() devuelve false
   cookie: {
     sameSite: 'none',
-    secure: true,
+    // secure: true,
+    httpOnly: true,
+    expires: 600000
   },
   secret: process.env.SESSION_SECRET,
   resave: false,
