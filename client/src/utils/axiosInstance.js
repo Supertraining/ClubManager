1742550b9afbf07bcf230116da1d66f9cec94eb9
MatíasJMaranga <marangamatias@gmail.com>
2023-10-26 22:28:56 -0300

    urlAxiosUpdate

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://club-manager-api-o1cp.onrender.co',
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
