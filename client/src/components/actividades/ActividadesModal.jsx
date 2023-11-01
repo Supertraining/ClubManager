f0df6e325a107ce30680c50d25513f15338c4f57
MatíasJMaranga <marangamatias@gmail.com>
2023-11-01 17:05:45 -0300

    activitiesStyles

--- client/src/components/actividades/ActividadesModal.jsx
+++ client/src/components/actividades/ActividadesModal.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
 
 const ActividadesModal = ({ category }) => {
-  console.log(category)
 
   return (
     <>
       { category?.map((category) => (
         <div key={ category.name }>
-          
-          <h6 className='fs-5 text-center text-decoration-underline'>
-            { category.days }
-          </h6>
 
-          <ul>
+          <ul className="d-flex flex-column align-items-center">
+            <h6 className='fs-5 text-decoration-underline'>
+              { category.days }
+            </h6>
             <li>
-              <b>{ category.name }: </b>
+              <b>Categoría: </b>
+              <i className='text-success'>
+                { category.name }
+              </i>
+            </li>
+            <li>
+              <b>Horario: </b>
               <i className='text-success'>
                 { category.schedule }
+              </i>
+            </li>
+            <li>
+              <b>Edad: </b>
+              <i className='text-success'>
+                { category.age_range }
               </i>
             </li>
           </ul>
--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -8,3 +8,4 @@ const instance = axios.create({
 
 export default instance;
 //https://club-manager-api-o1cp.onrender.com
+//http://localhost:8080
