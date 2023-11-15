ea58ca25cc39a31e213a4c4a52b5f48b3112d754
MatíasJMaranga <marangamatias@gmail.com>
2023-11-15 13:35:55 -0300

    welcomeTextForTestingApp

--- client/src/components/homeText/HomeText.jsx
+++ client/src/components/homeText/HomeText.jsx
@@ -12,8 +12,10 @@ const HomeText = () => {
         <h1>
           Bienvenidos a la app del Ranelagh Club!
         </h1>
-        <h2>¡Hacete una cuenta y reserva tu cancha desde donde estés!</h2>
-        <h5 className='lead text-success d-none d-md-block'>Obtene información sobre las actividades que brindamos en el club o comunicate con nosotros haciendo click en el botón de WhatsApp</h5>
+        <h2>Esta es una versión de prueba</h2>
+        <h5>Por favor, entra al menu, registrate y prueba la app</h5>
+        {/* <h2>¡Hacete una cuenta y reserva tu cancha desde donde estés!</h2> */}
+        {/* <h5 className='lead text-success d-none d-md-block'>Obtene información sobre las actividades que brindamos en el club o comunicate con nosotros haciendo click en el botón de WhatsApp</h5> */}
 
       </div>
 
